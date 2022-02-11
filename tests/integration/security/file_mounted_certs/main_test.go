b78d30f6dd3aab0650ad1f81728894f22510f894
Houssem El Fekih <houssem.elfekih@jetstack.io>
2022-02-11 11:04:41 +0000

    Fix typo in test helm value

--- tests/integration/security/file_mounted_certs/main_test.go
+++ tests/integration/security/file_mounted_certs/main_test.go
@@ -157,7 +157,7 @@ values:
     extraVolumeMounts:
       - name: server-certs
         mountPath: /server-certs
-    extraVolumeArgs:
+    extraArgs:
       - --caCertFile=/server-certs/root-cert.pem
       - --tlsCertFile=/server-certs/cert-chain.pem
       - --tlsKeyFile=/server-certs/key.pem
